5c48eb243adab103413d768eeb309d281a97e802
Putback101 <sophoea11@gmail.com>
2026-01-31 21:07:37 +0800

    Delete logistics-1 directory

--- logistics-1/config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Computes project root URL even if you are inside /views or /auth
-function app_base_url(): string {
-    $path = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])); // e.g. /logistics-1/views
-    if (str_ends_with($path, '/views')) $path = substr($path, 0, -5);
-    if (str_ends_with($path, '/auth'))  $path = substr($path, 0, -5);
-    return rtrim($path, '/');
-}
--- logistics-1/config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('BASE_PATH', realpath(__DIR__ . '/..'));
-
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-require_once BASE_PATH . '/config/database.php';
-require_once BASE_PATH . '/config/auth.php';
